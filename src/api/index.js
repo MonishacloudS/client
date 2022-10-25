48fe329315f81e08f00029cec2cdbd0367f0162b
MonishacloudS <monishas7122001@gmail.com>
2022-10-25 17:22:25 +0530

    final fixes

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://social-media-memories-mern.herokuapp.com' });
+const API = axios.create({ baseURL: 'https://social-media-memories-mern.herokuapp.com/posts' });
 
 //const url = 'https://social-media-memories-mern.herokuapp.com/posts';
 
